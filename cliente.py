69fe90563f8369f24d2b372697be5e2f370aff9f
Matheusdnf <matheusdiniz870@gmail.com>
2025-10-08 16:53:25 -0300

    feat: implementação requizição

--- cliente.py
+++ cliente.py
@@ -7,7 +7,7 @@ IP_SERVIDOR_RHCP = "127.0.0.1"
 # definindo a porta do servidor
 PORTA_SERVIDOR_RHCP = 8000
 
-def mensagem(metodo,objeto,campos):
+def mensagem(metodo:str,objeto:str,campos:str) -> bytes:
 #     METODO Objeto RHCP/1.0\r\n
 #     Campo: valor\r\n
 #     ...
@@ -31,7 +31,15 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((IP_SERVIDOR_RHCP, PORTA_SERVIDOR_RHCP))
 
 # enviando a requisicao
-msg_req = "MENSAGEM DE REQUISICAO AQUI".encode()
+msg_req = mensagem(
+    metodo="GET",
+    objeto="/recursos/usuarios",
+    campos={
+        "Host": "127.0.0.1",
+        "User-Agent": "ClienteRHCP/1.0",
+        "Accept": "text/plain"
+    }
+)
 s.send(msg_req)
 
 # recebendo a resposta
